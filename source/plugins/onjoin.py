fdc1232c36fb66076b8f4f4bb01e73507177ea84
maslychm <43874961+maslychm@users.noreply.github.com>
2019-02-28 20:27:19 -0500

    test on laptop logging not working for cp32

---
+++ source/plugins/onjoin.py
@@ -0,0 +1,8 @@
+from PIL import Image, ImageDraw, ImageFont
+
+im = Image.open("template.PNG")
+font_type = ImageFont.truetype("black.ttf",40)
+
+draw = ImageDraw.Draw(im)
+draw.text(xy=(100,100),text="Hello there!",fill=(17,61,193),font=font_type)
+im.show()
