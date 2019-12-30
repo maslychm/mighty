c06e48c608a0b2ac5d17071baec032bf088c018b
maslychm <maslychm@gmail.com>
2019-12-30 02:34:28 -0500

    adhering to naming conventions

--- mightybot.py
+++ mightybot.py
@@ -4,6 +4,8 @@ import modules.joinrelated as onjoin
 
 from credentials import TOKEN as TOKEN
 
+ADMIN_ID = 232346727221297164
+
 class Mighty(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}'.format(self.user))
@@ -11,7 +13,7 @@ class Mighty(discord.Client):
     async def on_message(self, message):
         print('Message from {0.author}: {0.content}'.format(message))
 
-        if message.content.startswith("close") and message.author.id == 232346727221297164:
+        if message.content.startswith("close") and message.author.id == ADMIN_ID:
             await message.channel.send("shutting down...")
             await client.close()
 
--- modules/joinrelated.py
+++ modules/joinrelated.py
@@ -4,7 +4,11 @@ import os
 import requests
 from io import BytesIO
 
-MAXSIZE = 300
+MAXPICSIZE = 300
+IMAGES_ALLOWED_LIST = [491899563527897089,491900012104646668,652345677942358026]
+JOIN_LEAVE_NOTIF_CHANNEL = 491900012104646668
+TEMP_WELCOME_PATH = "temp/tempImg.png"
+TEMP_HAT_PATH = "temp/temphat.png"
 
 def generate_onjoin_pic(namestr,url):
     # Open image file and font file
@@ -20,19 +24,18 @@ def generate_onjoin_pic(namestr,url):
     draw = ImageDraw.Draw(im)
     draw.text(xy=(185,70),text=namestr,fill=(17,17,19),font=font_type)
     im.paste(imageBG,box=(5,5))
-    im.save("temp/tempImg.png")
-    return "temp/tempImg.png"
+    im.save(TEMP_WELCOME_PATH)
 
 async def onjoin_welcome(client,member):
-    channel = client.get_channel(491900012104646668)
+    channel = client.get_channel(JOIN_LEAVE_NOTIF_CHANNEL)
     namestr = member.name
     if (member.nick):
         namestr = member.nick
-    image = generate_onjoin_pic(namestr,member.imageBG_url)
-    return await channel.send(file=discord.File(image))
+    generate_onjoin_pic(namestr,member.imageBG_url)
+    return await channel.send(file=discord.File(TEMP_WELCOME_PATH))
 
 async def on_leave(client,member):
-    channel = client.get_channel(491900012104646668)
+    channel = client.get_channel(JOIN_LEAVE_NOTIF_CHANNEL)
     namestr = member.name
     if (member.nick):
         namestr = member.nick
@@ -43,7 +46,6 @@ async def generate_hat(client,message):
     if not is_whitelisted(message):
         return
 
-    # Get the correct image based on request
     imageBG = None
     
     if message.attachments:
@@ -57,6 +59,7 @@ async def generate_hat(client,message):
         targetUser = message.mentions[0]
         response = requests.get(targetUser.avatar_url)
         imageBG = Image.open(BytesIO(response.content))
+        
     else:
         response = requests.get(message.author.avatar_url)
         imageBG = Image.open(BytesIO(response.content))
@@ -68,19 +71,19 @@ async def generate_hat(client,message):
     hat_w, hat_h = hatImg.size
     
     # Check if imageBG is too small and resize hat to 3:2
-    if bg_w < MAXSIZE or bg_h < MAXSIZE:
+    if bg_w < MAXPICSIZE or bg_h < MAXPICSIZE:
         hatImg.thumbnail((bg_w / 3 * 2, bg_h / 3 * 2), Image.ANTIALIAS)
         hat_w, hat_h = hatImg.size
     else:
-        imageBG.thumbnail((MAXSIZE,MAXSIZE), Image.ANTIALIAS)
+        imageBG.thumbnail((MAXPICSIZE, MAXPICSIZE), Image.ANTIALIAS)
         bg_w, bg_h = imageBG.size
 
     # Paste hat on correct offsets
     offset = ((bg_w - hat_w), 0)
     imageBG.paste(hatImg, offset, mask=hatImg)
-    imageBG.save("temp/temphat.png")
+    imageBG.save(TEMP_HAT_PATH)
     
-    return await message.channel.send(file=discord.File("temp/temphat.png"))
+    return await message.channel.send(file=discord.File(TEMP_HAT_PATH))
     
 async def test_welcome(client,message):
 
@@ -94,12 +97,11 @@ async def test_welcome(client,message):
             if message.author.nick:
                 namestr = message.author.nick
 
-    image = generate_onjoin_pic(namestr,message.author.avatar_url)
-    return await message.channel.send(file=discord.File(image))
+    generate_onjoin_pic(namestr,message.author.avatar_url)
+    return await message.channel.send(file=discord.File(TEMP_WELCOME_PATH))
 
 def is_whitelisted(message):
-    WHITELIST = [491899563527897089,491900012104646668,652345677942358026]
     if not (isinstance(message.channel,discord.DMChannel)
-    or message.channel.id in WHITELIST):
+    or message.channel.id in IMAGES_ALLOWED_LIST):
         return False
     return True
