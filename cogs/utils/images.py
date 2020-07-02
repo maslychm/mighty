04d09d53f2df32e8ab98f75b2ac0596e41a79d75
maslychm <maslychm@gmail.com>
2020-07-02 00:25:09 -0400

    limit name char len to 12 for image generation

--- cogs/utils/images.py
+++ cogs/utils/images.py
@@ -30,7 +30,7 @@ def generate_onjoin_pic(namestr: str, member_id: int, url):
 
     # Add text user name to image
     draw = ImageDraw.Draw(im)
-    draw.text(xy=(185,75),text=namestr.lower(),fill=(17,17,19),font=font_type)
+    draw.text(xy=(185,75),text=namestr[:12],fill=(17,17,19),font=font_type)
     im.paste(imageBG,box=(5,5))
     im.save(retpath)
 
@@ -76,7 +76,7 @@ def generate_hat(member_id, url=None):
 
 
 def generate_nuzzle(a, m, a_url, m_url, reversed=False):
-    nuzzlestr = a + " *nuzzles* " + m
+    nuzzlestr = a[:12] + " *nuzzles* " + m[:12]
     retpath = "fpath"
 
     box_author = (360,130)
